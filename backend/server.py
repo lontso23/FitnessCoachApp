67ffb88283ab24b17cd0b47c76b10ab8c984dc71
Mikel Nieto Irureta <mikel.lontso@gmail.com>
2026-02-03 17:15:29 +0100

    Fix CORS

--- backend/server.py
+++ backend/server.py
@@ -654,10 +654,17 @@ async def root():
 # Include router
 app.include_router(api_router)
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://macrobuilder.netlify.app",
+    ],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
